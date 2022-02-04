6a60072cbe2eccb94127965362dfa65c64e86f81
Avvy <saberwatch@gmail.com>
2022-02-04 01:04:18 -0800

    '

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,9 +19,9 @@ const Home: NextPage = () => {
             <p className={styles.hook}>SO, YOU WANT TO TRAVEL TO</p>
             <h1>SPACE</h1>
             <p>
-              Let's face it; if you want to go to space, you might as well genuinely go to outer
-              space and not hover kind of on the edge of it. Well sit back, and relax because we'll
-              give you a truly out of this world experience!
+              Let&apos;s face it; if you want to go to space, you might as well genuinely go to
+              outer space and not hover kind of on the edge of it. Well sit back, and relax because
+              we&apos;ll give you a truly out of this world experience!
             </p>
           </div>
           <NextLink href="/destination" passHref>
