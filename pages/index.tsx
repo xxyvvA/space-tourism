2eabfa7b47df70991ea52aaa41dfd330d983666d
Avvy <saberwatch@gmail.com>
2022-02-05 16:45:55 -0800

    uneeded imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
-import { useState } from "react";
 import type { NextPage } from "next";
-import Image from "next/image";
 import Head from "../components/Head";
 import Navigation from "../components/Navigation";
 import NextLink from "next/link";
--- pages/technology.tsx
+++ pages/technology.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import type { NextPage } from "next";
-import Image from "next/image";
 import Head from "../components/Head";
 import Navigation from "../components/Navigation";
 import styles from "../styles/Home.module.scss";
